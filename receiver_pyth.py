83b44ebca35fa5a8f70de306e8ebe8bbe12d05f0
Franciszek Badziak <132668769+badziak@users.noreply.github.com>
2024-03-23 18:55:11 +0100

    Update receiver_pyth.py

--- receiver_pyth.py
+++ receiver_pyth.py
@@ -1,44 +1,33 @@
-import re
-import serial
+#include <SPI.h>
+#include <LoRa.h>
 
-def LoRa(COM_PORT):
-    # Define the COM port and baud rate
-    BAUD_RATE = 9600
-    #COM_PORT = 'COM9'
-    # Open the serial port
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
 
-    try:
-        ser = serial.Serial(COM_PORT, BAUD_RATE)
-        # Read a line of data from the serial port
-        data = ser.readline().decode('latin1').strip()
-        data_labels = data
+  Serial.println("LoRa Receiver");
 
-        # Split the string into individual labels
-        labels_list = data_labels.split(', ')
-        if data is not None:
-            if len(labels_list) > 13:
-                # Create a dictionary to map labels to column indices
-                try:
-                    label_to_index = {label.split()[0]: idx for idx, label in enumerate(labels_list)}
-                    pattern = re.compile(r'\d+(\.\d+)?')
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
 
-                    # Remove non-numeric characters and convert to float
-                    filtered_data = [float(pattern.search(item).group()) for item in labels_list if pattern.search(item)]
-                    return filtered_data
-                except Exception as e:
-                    LoRa(COM_PORT)
-                    return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            else:
-                return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        else:
-            return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+void loop() {
+  // try to parse packet
+  
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    //Serial.print("Received packet ");
 
-    except serial.SerialException:
-        # Close the serial port when Ctrl+C is pressed
-        return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
 
-
-
-
-
-
+    // print RSSI of packet
+    Serial.print(",");
+    Serial.println(LoRa.packetRssi());
+  }
+}
